a4fc51a9dc85b1087ef62a72d5d4e0ca7d1d8c84
B00mlex <B00mlex@users.noreply.github.com>
2020-12-04 21:08:37 +0800

    first

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('.burger').click(function(event) {
+        $('.burger,.menu').toggleClass('active');
+    });
+
+    $('.sub-m').click(function(event) {
+        $('.sub-mul').toggleClass('open');
+    });
+});
