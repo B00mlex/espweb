15f73dfb15eb055dd28f01ef1ad88d8de5d9fbe1
B00mlex <B00mlex@users.noreply.github.com>
2020-12-06 21:37:39 +0800

    radio

--- js/script.js
+++ js/script.js
@@ -12,16 +12,32 @@ $(document).ready(function () {
        }
     });
 
-    $(document).on('click','.checkbox', function(event){
-        if ($(this).hasClass('active')){
-            $(this).find('input').prop('checked, true');
-        }else{
-            $(this).find('input').prop('checked, false');
-        }
+    $('.checkbox').click( function(event){
         $(this).toggleClass('active');
-        return true;
+        if ($(this).hasClass('active')){
+            $(this).find('input').prop('checked', true);
+        }else{
+            $(this).find('input').prop('checked', false);
+        }
     });
 
     //====END CHECKBOX=======
+
+//======== RADIO ===============
+$.each($('.options_item'), function(){
+    if ($(this).find('input').prop('checked')==true ){
+        $(this).addClass('active');
+    }
+ });
+ $('.options_item').click( function(event){
+    if ($(this).find('input').prop('checked')==false ){
+        $(this).parents('.options').find('.options_item').removeClass('active');
+        $(this).parents('.options').find('.options_item .input').prop('checked', false);
+        $(this).find('input').prop('checked', true);
+        $(this).addClass('active');
+      }
+});
+
+//======== END RADIO ===========
 
 });//============ end document ready===========
