a5cea36c61a0b4c80786e80e38708b0efed42f20
B00mlex <B00mlex@users.noreply.github.com>
2020-12-06 16:59:08 +0800

    checkbox

--- js/script.js
+++ js/script.js
@@ -4,7 +4,24 @@ $(document).ready(function () {
         $('.burger,.menu').toggleClass('active');
     });
 
-    $('.sub-m').click(function (event) {
-        $('.sub-mul').toggleClass('open');
+    //==== CHECKBOX=======
+
+    $.each($('.checkbox'), function(index, val){
+       if ($(this).find('input').prop('checked')==true ){
+           $(this).addClass('active');
+       }
     });
-});
+
+    $(document).on('click','.checkbox', function(event){
+        if ($(this).hasClass('active')){
+            $(this).find('input').prop('checked, true');
+        }else{
+            $(this).find('input').prop('checked, false');
+        }
+        $(this).toggleClass('active');
+        return true;
+    });
+
+    //====END CHECKBOX=======
+
+});//============ end document ready===========
